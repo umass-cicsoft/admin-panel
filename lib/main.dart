ec67f311f62250345d0f07ab74aa6b312b638cb1
oorischubert <61707814+oorischubert@users.noreply.github.com>
2022-04-06 02:24:10 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,18 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
-  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+  final Future<FirebaseApp> _server = Firebase.initializeApp();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Admin Panel',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch: Colors.blue,
         ),
         home: FutureBuilder(
-            future: _fbApp,
+            future: _server,
             builder: (context, snapshot) {
               if (snapshot.hasError) {
                 //if error occurs return this!
@@ -40,7 +31,7 @@ class MyApp extends StatelessWidget {
                 return const Text("Oops, something went wrong!");
               } else if (snapshot.hasData) {
                 //if data returned load up home page!
-                return  HomePage();
+                return  const HomePage();
               } else {
                 //if no data has been returned yet show loading screen!
                 return const Center(
