c45a17352e3b9ba29abaf18d4ab3c69b88675e69
AVS1508 <aditya.11.present@gmail.com>
2023-02-03 13:43:20 +0530

    Configured Navigation Sidebar to use icons for display, along collapsibility addition

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -6,6 +6,23 @@ import {
   SubMenu,
   useProSidebar,
 } from "react-pro-sidebar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+  faHouse,
+  faUsers,
+  faPeopleGroup,
+  faUserSecret,
+  faToolbox,
+  faUserPen,
+  faUserCheck,
+  faMagnifyingGlass,
+  faPenToSquare,
+  faWarehouse,
+  faHashtag,
+  faChalkboard,
+} from "@fortawesome/free-solid-svg-icons";
 import { CICSoftLogo, FallbackCICSoftLogo } from "../assets";
 import { Image } from "../utils";
 
@@ -16,10 +33,20 @@ const StyledContainer = styled.div`
     props.direction ? "rtl" : "ltr"};
 `;
 
+const StyledSpacingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-between;
+`;
+
 const StyledMenu = styled(Menu)`
   width: 100%;
   font-size: large;
-  padding: 0.5rem;
+
+  .ps-submenu-content {
+    width: min-content;
+  }
 `;
 
 const StyledSubMenu = styled(SubMenu)`
@@ -37,46 +64,88 @@ const StyledMenuLogo = styled(MenuItem)`
   display: flex;
   width: 100%;
   text-align: center;
+  padding: 0.5em 0em;
 `;
 
 export default function NavigationBar() {
-  const { toggleSidebar, collapseSidebar, broken, rtl } = useProSidebar();
+  const { collapseSidebar, collapsed, rtl } = useProSidebar();
   return (
     <StyledContainer direction={rtl}>
-      <Sidebar breakPoint="lg">
-        <StyledMenu>
-          <StyledMenuLogo>
-            <Image
-              src={FallbackCICSoftLogo}
-              srcSet={CICSoftLogo}
-              alt="CICSoft Logo"
-              width="100%"
-              height="auto"
-            ></Image>
-          </StyledMenuLogo>
-          <StyledMenuItem>Dashboard</StyledMenuItem>
-          <StyledSubMenu label="Members">
-            <StyledMenuItem>Manage Members</StyledMenuItem>
-            <StyledMenuItem>Verified Members</StyledMenuItem>
-            <StyledMenuItem>Search Members</StyledMenuItem>
-          </StyledSubMenu>
-          <StyledSubMenu label="Core Team">
-            <StyledMenuItem>Manage Teams</StyledMenuItem>
-            <StyledMenuItem>Logistics Team</StyledMenuItem>
-            <StyledMenuItem>Social Media Team</StyledMenuItem>
-            <StyledMenuItem>Teaching Team</StyledMenuItem>
-          </StyledSubMenu>
-          <StyledMenuItem>Development Team</StyledMenuItem>
-          <StyledMenuItem>Administration</StyledMenuItem>
-        </StyledMenu>
+      <Sidebar customBreakPoint="0px" width="270px" collapsed={collapsed}>
+        <StyledSpacingContainer>
+          <div>
+            <StyledMenu>
+              {collapsed ? (
+                <StyledMenuItem
+                  icon={<FontAwesomeIcon icon={faAngleDoubleRight} />}
+                  onClick={() => collapseSidebar(false)}
+                ></StyledMenuItem>
+              ) : (
+                <MenuItem
+                  suffix={<FontAwesomeIcon icon={faAngleDoubleLeft} />}
+                  onClick={() => collapseSidebar(true)}
+                >
+                  <StyledMenuLogo>
+                    <Image
+                      src={FallbackCICSoftLogo}
+                      srcSet={CICSoftLogo}
+                      alt="CICSoft Logo"
+                      width="100%"
+                      height="auto"
+                    />
+                  </StyledMenuLogo>
+                </MenuItem>
+              )}
+            </StyledMenu>
+            <hr style={{ margin: "0" }} />
+            <StyledMenu>
+              <StyledMenuItem icon={<FontAwesomeIcon icon={faHouse} />}>
+                Dashboard
+              </StyledMenuItem>
+              <StyledSubMenu
+                label="Members"
+                icon={<FontAwesomeIcon icon={faUsers} />}
+              >
+                <StyledMenuItem icon={<FontAwesomeIcon icon={faUserPen} />}>
+                  Manage Members
+                </StyledMenuItem>
+                <StyledMenuItem icon={<FontAwesomeIcon icon={faUserCheck} />}>
+                  Verified Members
+                </StyledMenuItem>
+                <StyledMenuItem
+                  icon={<FontAwesomeIcon icon={faMagnifyingGlass} />}
+                >
+                  Search Members
+                </StyledMenuItem>
+              </StyledSubMenu>
+              <StyledSubMenu
+                label="Core Team"
+                icon={<FontAwesomeIcon icon={faPeopleGroup} />}
+              >
+                <StyledMenuItem icon={<FontAwesomeIcon icon={faPenToSquare} />}>
+                  Manage Teams
+                </StyledMenuItem>
+                <StyledMenuItem icon={<FontAwesomeIcon icon={faWarehouse} />}>
+                  Logistics Team
+                </StyledMenuItem>
+                <StyledMenuItem icon={<FontAwesomeIcon icon={faHashtag} />}>
+                  Social Media Team
+                </StyledMenuItem>
+                <StyledMenuItem icon={<FontAwesomeIcon icon={faChalkboard} />}>
+                  Teaching Team
+                </StyledMenuItem>
+              </StyledSubMenu>
+              <StyledMenuItem icon={<FontAwesomeIcon icon={faUserSecret} />}>
+                Development Team
+              </StyledMenuItem>
+              <StyledMenuItem icon={<FontAwesomeIcon icon={faToolbox} />}>
+                Administration
+              </StyledMenuItem>
+            </StyledMenu>
+          </div>
+          <StyledMenu>{/* Placeholder for Sidebar Footer */}</StyledMenu>
+        </StyledSpacingContainer>
       </Sidebar>
-      <main>
-        <div style={{ display: "flex", padding: 10 }}>
-          {broken ? (
-            <button onClick={() => toggleSidebar()}>Toggle </button>
-          ) : null}
-        </div>
-      </main>
     </StyledContainer>
   );
 }
