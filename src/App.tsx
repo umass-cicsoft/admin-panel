678b8940850f9d472a07817c14dc6a12a8c6bf52
AVS1508 <aditya.11.present@gmail.com>
2023-02-13 17:27:30 -0500

    Updated the App component to use Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-import React from "react";
-import styled from "styled-components";
 import { ProSidebarProvider } from "react-pro-sidebar";
-
 import { NavigationBar } from "./components";
-
-const AppContainer = styled.div`
-  text-align: center;
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-`;
 
 export default function App() {
   return (
     <ProSidebarProvider>
-      <AppContainer>
+      <div className="flex h-screen w-screen text-center">
         <NavigationBar />
-      </AppContainer>
+      </div>
     </ProSidebarProvider>
   );
 }
