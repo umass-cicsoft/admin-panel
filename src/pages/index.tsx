d8e7e7237bb11e5db18a3afe00097b23420c4832
AVS1508 <aditya.11.present@gmail.com>
2023-02-15 16:34:19 -0500

    Initialized basic routing pages: Administration, Dashboard, Members

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,3 @@
-export {};
+export { default as Administration } from "./Administration/Administration";
+export { default as Dashboard } from "./Dashboard/Dashboard";
+export { default as Members } from "./Members/Members";
