03047942104073129dc1eb3fc9563d6b8f08e7ce
AVS1508 <aditya.11.present@gmail.com>
2023-02-15 16:46:53 -0500

    Added outlet for nested routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ProSidebarProvider } from "react-pro-sidebar";
 import { NavigationBar } from "./components";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
   return (
     <ProSidebarProvider>
       <div className="flex h-screen w-screen text-center">
         <NavigationBar />
+        <Outlet />
       </div>
     </ProSidebarProvider>
   );
